1a8192ad0e956e87d3d6a49a2d493ca0097cf7a5
Karen Freitas <karenfcorrea@gmail.com>
2021-08-20 14:20:59 -0300

    arrumando foto de perfil no navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -12,8 +12,8 @@ export const navbar = () =>{
   <button class="menu-mobile-btn" id="home-navbar"><img src="./img/home-navbar.png" class="menu-img"></button> 
   <button class="menu-mobile-btn" id="add-review-navbar"><img src="./img/add-navbar.png" class="menu-img" id="add-review-navbar"></button> 
   <button class="menu-mobile-btn" id="saved-navbar"><img src="./img/save-navbar.png" class="menu-img" id="save-navbar-img"></button>    
-  <button class="menu-mobile-btn" id="profile-navbar"><img src="${photoNavbar}" class="menu-img" id="photo-navbar" ></button> 
   <button class="menu-mobile-btn" id="open-sidebar"><img src="./img/menu-navbar.png" class="menu-img" ></button>  
+  <button class="menu-mobile-btn" id="profile-navbar"><img src="${photoNavbar}" class="menu-img" id="photo-navbar" ></button> 
   `
   sectionElement.innerHTML = navbarTemplate
 
